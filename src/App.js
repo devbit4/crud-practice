56ba7ef552886d6e2d1978e2a66e94e3b0e061e1
devbit4 <devbit4@gmail.com>
2022-02-19 01:05:16 +0900

    추가 및 삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const [helps, setHelps] = useState([]);
+  const writer = useRef(null);
+  const content = useRef(null);
+
+  console.log(helps);
+
+  // 추가
+  const Addhandlement = () => {
+    if (!writer.current.value || !content.current.value) return;
+    setHelps([{
+      writer: writer.current.value,
+      content: content.current.value,
+    },
+    ...helps
+    ]);
+  }
+  // 삭제
+  const deletehandlement = (deleteIndex) => {
+    setHelps(helps.filter((help, helpIndex) => helpIndex !== deleteIndex))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='write'>
+        <input type="text" placeholder='writer' ref={writer} />
+        <input type="text" placeholder='content' ref={content} />
+        <button onClick={Addhandlement}>CLICK!</button>
+
+      </section>
+      <section className='show'>
+        {helps.map((help, index) => {
+          return (<article key={index}>
+            <h1>{help.writer}</h1>
+            <p>{help.content}</p>
+            <button onClick={() => deletehandlement(index)}>지우개</button>
+          </article>)
+        })
+        }
+      </section>
+    </>
   );
 }
 
