78d9d1cefae0b810cc217a9c82e4a55abcb1c126
devbit4 <devbit4@gmail.com>
2022-02-23 08:41:51 +0900

    수정모드(업데이트/수정취소) 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import './App.css';
 
 function App() {
 
-  const [helps, setHelps] = useState([]);
+  const [helps, setHelps] = useState([{ writer: "bitna", content: "hello" }]);
   const writer = useRef(null);
   const content = useRef(null);
-
-  console.log(helps);
-
+  const updateWriter = useRef(null);
+  const updateContent = useRef(null);
   // 추가
   const Addhandlement = () => {
     if (!writer.current.value || !content.current.value) return;
@@ -24,6 +23,39 @@ function App() {
   const deletehandlement = (deleteIndex) => {
     setHelps(helps.filter((help, helpIndex) => helpIndex !== deleteIndex))
   }
+
+  // 수정
+  const setUpdatemode = (index) => {
+    setHelps(helps.map((help, helpIndex) => {
+      if (index === helpIndex) help.update = true;
+      return help;
+
+    }))
+  }
+
+  const setCancelmode = (index) => {
+    setHelps(helps.map((help, helpIndex) => {
+      if (index === helpIndex) help.update = false;
+      return help;
+
+    }))
+  }
+
+  const setSavemode = (index) => {
+    // console.log(updateWriter.current.value);
+    setHelps(helps.map((help, helpIndex) => {
+      if (index === helpIndex) {
+
+        help.writer = updateWriter.current.value;
+        help.content = updateContent.current.value;
+        help.update = false;
+      }
+      return help;
+
+    }))
+  }
+
+
   return (
     <>
       <section className='write'>
@@ -34,11 +66,30 @@ function App() {
       </section>
       <section className='show'>
         {helps.map((help, index) => {
-          return (<article key={index}>
-            <h1>{help.writer}</h1>
-            <p>{help.content}</p>
-            <button onClick={() => deletehandlement(index)}>지우개</button>
-          </article>)
+          return (
+
+            <article key={index} >
+              {
+                help.update ?
+                  <>
+                    <input type="text" defaultValue={help.writer} ref={updateWriter} />
+                    <input type="text" defaultValue={help.content} ref={updateContent}></input>
+                    <button onClick={() => setSavemode(index)}>확인</button>
+                    <button onClick={() => setCancelmode(index)}>취소</button>
+                  </>
+                  :
+                  <>
+                    <h1>{help.writer}</h1>
+                    <p>{help.content}</p>
+                    <button onClick={() => setUpdatemode(index)}>수정모드</button>
+                    <button onClick={() => deletehandlement(index)}>지우개</button>
+                  </>
+              }
+
+            </article>
+
+
+          )
         })
         }
       </section>
